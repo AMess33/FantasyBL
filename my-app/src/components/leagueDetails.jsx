2db8343095d254cd14c44ddc9388d835d206ba2f
Andrew M <amess3309@gmail.com>
2024-02-04 23:13:09 -0600

    import material components to use and set up basic form structure of material components

--- my-app/src/components/leagueDetails.jsx
+++ my-app/src/components/leagueDetails.jsx
@@ -1,4 +1,27 @@
 import React from "react";
-
+import {
+  FormControl,
+  Select,
+  Box,
+  InputLabel,
+  MenuItem,
+  TextField,
+} from "@mui/material";
 // return league name/ sport/ league/ league type
-function LeagueDetails() {}
+function LeagueDetails() {
+  return (
+    <Box>
+      <FormControl>
+        <InputLabel></InputLabel>
+        <TextField></TextField>
+        <InputLabel></InputLabel>
+        <Select>
+          <MenuItem></MenuItem>
+          <MenuItem></MenuItem>
+          <MenuItem></MenuItem>
+          <MenuItem></MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
