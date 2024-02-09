044cd4dd90411049a2a5987b634eee36f08f3ada
Andrew M <amess3309@gmail.com>
2024-02-08 19:00:31 -0600

    add values to each selection

--- my-app/src/components/lineupSpots.jsx
+++ my-app/src/components/lineupSpots.jsx
@@ -16,32 +16,32 @@ function FootballLineup() {
       <FormControl>
         <label>How many starters for each position?</label>
         <InputLabel>QB</InputLabel>
-        <Select>1</Select>
-        <Select>2</Select>
-        <Select>3</Select>
+        <Select value={1}>1</Select>
+        <Select value={2}>2</Select>
+        <Select value={3}>3</Select>
         <InputLabel>RB</InputLabel>
-        <Select>1</Select>
-        <Select>2</Select>
-        <Select>3</Select>
-        <Select>4</Select>
+        <Select value={1}>1</Select>
+        <Select value={2}>2</Select>
+        <Select value={3}>3</Select>
+        <Select value={4}>4</Select>
         <InputLabel>WR</InputLabel>
-        <Select>1</Select>
-        <Select>2</Select>
-        <Select>3</Select>
-        <Select>4</Select>
+        <Select value={1}>1</Select>
+        <Select value={2}>2</Select>
+        <Select value={3}>3</Select>
+        <Select value={4}>4</Select>
         <Select>5</Select>
         <InputLabel>TE</InputLabel>
-        <Select>1</Select>
-        <Select>2</Select>
-        <Select>3</Select>
+        <Select value={1}>1</Select>
+        <Select value={2}>2</Select>
+        <Select value={3}>3</Select>
         <InputLabel>SuperFlex (QB/RB/WR/TE)</InputLabel>
-        <Select>1</Select>
-        <Select>2</Select>
+        <Select value={1}>1</Select>
+        <Select value={2}>2</Select>
         <InputLabel>Flex (RB/WR/TE)</InputLabel>
-        <Select>1</Select>
-        <Select>2</Select>
-        <Select>3</Select>
-        <Select>4</Select>
+        <Select value={1}>1</Select>
+        <Select value={2}>2</Select>
+        <Select value={3}>3</Select>
+        <Select value={4}>4</Select>
         <Select>5</Select>
       </FormControl>
     </Box>
@@ -55,45 +55,45 @@ function BasketballLineup() {
       <FormControl>
         <label>How many starters for each position?</label>
         <InputLabel>Point Gaurd</InputLabel>
-        <Select>1</Select>
-        <Select>2</Select>
-        <Select>3</Select>
+        <Select value={1}>1</Select>
+        <Select value={2}>2</Select>
+        <Select value={3}>3</Select>
         <InputLabel>Shooting Gaurd</InputLabel>
-        <Select>1</Select>
-        <Select>2</Select>
-        <Select>3</Select>
-        <Select>4</Select>
+        <Select value={1}>1</Select>
+        <Select value={2}>2</Select>
+        <Select value={3}>3</Select>
+        <Select value={4}>4</Select>
         <InputLabel>Small Forward</InputLabel>
-        <Select>1</Select>
-        <Select>2</Select>
-        <Select>3</Select>
-        <Select>4</Select>
-        <Select>5</Select>
+        <Select value={1}>1</Select>
+        <Select value={2}>2</Select>
+        <Select value={3}>3</Select>
+        <Select value={4}>4</Select>
+        <Select value={5}>5</Select>
         <InputLabel>Power Forward</InputLabel>
-        <Select>1</Select>
-        <Select>2</Select>
-        <Select>3</Select>
+        <Select value={1}>1</Select>
+        <Select value={2}>2</Select>
+        <Select value={3}>3</Select>
         <InputLabel>Center</InputLabel>
-        <Select>1</Select>
-        <Select>2</Select>
-        <Select>3</Select>
+        <Select value={1}>1</Select>
+        <Select value={2}>2</Select>
+        <Select value={3}>3</Select>
         <InputLabel>Guard (PG/SG)</InputLabel>
-        <Select>1</Select>
-        <Select>2</Select>
-        <Select>3</Select>
-        <Select>4</Select>
-        <Select>5</Select>
+        <Select value={1}>1</Select>
+        <Select value={2}>2</Select>
+        <Select value={3}>3</Select>
+        <Select value={4}>4</Select>
+        <Select value={5}>5</Select>
         <InputLabel>Forward (SF/PF)</InputLabel>
-        <Select>1</Select>
-        <Select>2</Select>
-        <Select>3</Select>
-        <Select>4</Select>
-        <Select>5</Select>
+        <Select value={1}>1</Select>
+        <Select value={2}>2</Select>
+        <Select value={3}>3</Select>
+        <Select value={4}>4</Select>
+        <Select value={5}>5</Select>
         <InputLabel>Utility Player (PG/SG/SF/PF/C)</InputLabel>
-        <Select>1</Select>
-        <Select>2</Select>
-        <Select>3</Select>
-        <Select>4</Select>
+        <Select value={1}>1</Select>
+        <Select value={2}>2</Select>
+        <Select value={3}>3</Select>
+        <Select value={4}>4</Select>
       </FormControl>
     </Box>
   );
