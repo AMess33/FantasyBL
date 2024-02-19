2fa9e6ee6464c6bedd4aebdf1b348e907eed61e0
Andrew M <amess3309@gmail.com>
2024-02-18 20:06:28 -0600

    base trades component

--- my-app/src/components/GeneralSettings/trades.jsx
+++ my-app/src/components/GeneralSettings/trades.jsx
@@ -7,3 +7,16 @@ import {
   MenuItem,
   TextField,
 } from "@mui/material";
+
+function Trades() {
+  return (
+    <Box>
+      <FormControl>
+        <label>What is able to be traded in your league?</label>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </FormControl>
+    </Box>
+  );
+}
+
+export default Trades;
