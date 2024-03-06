cf391384e2dccf037e448713e448038a9cd5c10f
Andrew M <amess3309@gmail.com>
2024-03-06 17:01:21 -0600

    begin importing components to app page for display

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import LeagueDetails from "./components/InitialSetup/leagueDetails";
+import LeagueType from "./components/InitialSetup/leagueType";
+import LeagueSetup from "./components/InitialSetup/leagueSetup";
+import LeagueStyle from "./components/InitialSetup/leagueStyle";
+import OwnerFees from "./components/InitialSetup/ownerFees";
+import PlacesPaidOut from "./components/InitialSetup/placesPaidOut";
+import NumberOfTeams from "./components/InitialSetup/numberOfTeams";
+import RosterSize from "./components/InitialSetup/rosterSize";
+import TeamName from "./components/InitialSetup/teamName";
+import TeamLogo from "./components/InitialSetup/teamLogo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LeagueDetails />
+        <LeagueType />
+        <LeagueSetup />
+        <LeagueStyle />
+        <OwnerFees />
+        <PlacesPaidOut />
+        <NumberOfTeams />
+        <RosterSize />
+        <TeamName />
+        <TeamLogo />
       </header>
     </div>
   );
