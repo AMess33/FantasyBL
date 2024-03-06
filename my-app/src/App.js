949ebe3733734141d1642e4f558f9e3a1e487a66
Andrew M <amess3309@gmail.com>
2024-03-06 17:40:19 -0600

    fix import pathing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 import React from "react";
 import LeagueDetails from "./components/InitialSetup/leagueDetails";
 import LeagueType from "./components/InitialSetup/leagueType";
-import LeagueStyle from "./components/InitialSetup/leagueStyle";
-import OwnerFees from "./components/InitialSetup/ownerFees";
-import PlacesPaidOut from "./components/InitialSetup/placesPaidOut";
-import NumberOfTeams from "./components/InitialSetup/numberOfTeams";
-import RosterSize from "./components/InitialSetup/rosterSize";
+import {
+  LeagueStyle,
+  OwnerFees,
+  PlacesPaidOut,
+  NumberOfTeams,
+  RosterSize,
+} from "./components/InitialSetup/leagueSetup";
 import Commish from "./components/GeneralSettings/commish";
 import Owner from "./components/GeneralSettings/owner";
 import Trades from "./components/GeneralSettings/trades";
 import Drafts from "./components/GeneralSettings/drafts";
 import Scoring from "./components/GeneralSettings/scoring";
-import Waivers from "./components/GeneralSettings/waivers";
+import Waivers from "./components/GeneralSettings/standings";
 import Standings from "./components/GeneralSettings/standings";
 import Lineup from "./components/GeneralSettings/lineup";
 function App() {
--- my-app/src/components/GeneralSettings/lineup.jsx
+++ my-app/src/components/GeneralSettings/lineup.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import baseballLineup from "./baseballLineup";
-import basketballLineup from "./basketballLineup";
-import footballLineup from "./footballLineup";
-import hockeyLineup from "./hockeyLineup";
+import BaseballLineup from "./Baseball/baseballLineup";
+import BasketballLineup from "./Basketball/basketballLineup";
+import FootballLineup from "./Football/footballLineup";
+import HockeyLineup from "./Hockey/hockeyLineup";
 
 function Lineup() {
   return (
     <Box>
       {/* display the lineup based on what was selected from leaugeDetails form */}
-      <baseballLineup />
-      <basketballLineup />
-      <footballLineup />
-      <hockeyLineup />
+      <BaseballLineup />
+      <BasketballLineup />
+      <FootballLineup />
+      <HockeyLineup />
     </Box>
   );
 }
