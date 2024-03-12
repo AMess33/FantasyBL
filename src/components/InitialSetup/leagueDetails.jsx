5c2414a0d39f38c82e43418d31c972af83f47b55
Andrew M <amess3309@gmail.com>
2024-03-11 21:49:39 -0500

    update material componenets in league details

--- src/components/InitialSetup/leagueDetails.jsx
+++ src/components/InitialSetup/leagueDetails.jsx
@@ -12,55 +12,59 @@ function LeagueDetails(props) {
   // setValues(prevValues => ({...prevValues, thisValue: event.target.value }))
   // }
   return (
-    <Box>
-      <FormControl>
-        <InputLabel>Name of your League</InputLabel>
-        <TextField
-          id="leagueName"
-          value={props.value.leagueName}
-          label="League Name"
-          variant="filled"
-        />
-        <InputLabel>Sports League</InputLabel>
-        <Select
-          labelId="Sport"
-          id="Sport"
-          label="Sport"
-          value={props.value.sport}
-          onChange={(event) => {
-            props.setValue((prevValues) => ({
-              ...prevValues,
-              sport: event.target.value,
-            }));
-          }}
-        >
-          <MenuItem value={"NFL"}>NFL</MenuItem>
-          <MenuItem value={"NBA"}>NBA</MenuItem>
-          <MenuItem value={"NHL"}>NHL</MenuItem>
-          <MenuItem value={"MLB"}>MLB</MenuItem>
-          <MenuItem value={"NCAA Football"}>NCAA Football</MenuItem>
-        </Select>
-        <InputLabel>Hosting Website</InputLabel>
-        <Select
-          labelId="HostSite"
-          id="HostSite"
-          value={props.value.website}
-          label="Host"
-        >
-          <MenuItem value={"ESPN"}>ESPN</MenuItem>
-          <MenuItem value={"Sleeper"}>Sleeper</MenuItem>
-          <MenuItem value={"My Fantasy League"}>My Fantasy League</MenuItem>
-          <MenuItem value={"FanTrax"}>FanTrax</MenuItem>
-          <MenuItem value={"Yahoo"}>Yahoo!</MenuItem>
-          <MenuItem value={"FFPC"}>FFPC</MenuItem>
-          <MenuItem value={"CBS Sports"}>CBS Sports</MenuItem>
-          <MenuItem value={"FanDuel"}>FanDuel</MenuItem>
-          <MenuItem value={"DraftKings"}>DraftKings</MenuItem>
-          <MenuItem value={"FFPC"}>FFPC</MenuItem>
-          <MenuItem value={"Other"}>Other</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
+    <div>
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl sx={{ width: "33%" }}>
+          <h3>What is your League Name?</h3>
+          <TextField
+            id="leagueName"
+            value={props.value.leagueName}
+            label="League Name"
+            variant="filled"
+          />
+        </FormControl>
+      </Box>
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl sx={{ width: "33%" }}>
+          <h3>Select a Sport</h3>
+          <Select
+            id="sport"
+            variant="filled"
+            value={props.value.sport}
+            onChange={(event) => {
+              props.setValue((prevValues) => ({
+                ...prevValues,
+                sport: event.target.value,
+              }));
+            }}
+          >
+            <MenuItem value={"NFL"}>NFL</MenuItem>
+            <MenuItem value={"NBA"}>NBA</MenuItem>
+            <MenuItem value={"NHL"}>NHL</MenuItem>
+            <MenuItem value={"MLB"}>MLB</MenuItem>
+            <MenuItem value={"NCAA Football"}>NCAA Football</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl sx={{ width: "33%" }}>
+          <h3>What Website is your leauge using?</h3>
+          <Select id="hostSite" variant="filled" value={props.value.website}>
+            <MenuItem value={"ESPN"}>ESPN</MenuItem>
+            <MenuItem value={"Sleeper"}>Sleeper</MenuItem>
+            <MenuItem value={"My Fantasy League"}>My Fantasy League</MenuItem>
+            <MenuItem value={"FanTrax"}>FanTrax</MenuItem>
+            <MenuItem value={"Yahoo"}>Yahoo!</MenuItem>
+            <MenuItem value={"FFPC"}>FFPC</MenuItem>
+            <MenuItem value={"CBS Sports"}>CBS Sports</MenuItem>
+            <MenuItem value={"FanDuel"}>FanDuel</MenuItem>
+            <MenuItem value={"DraftKings"}>DraftKings</MenuItem>
+            <MenuItem value={"FFPC"}>FFPC</MenuItem>
+            <MenuItem value={"Other"}>Other</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+    </div>
   );
 }
 
