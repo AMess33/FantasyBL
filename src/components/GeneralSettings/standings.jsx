abf8015b14de3dc10859ea39c3439a2ab2cf204e
Andrew M <amess3309@gmail.com>
2024-03-14 20:20:24 -0500

    update attributes in trades

--- src/components/GeneralSettings/standings.jsx
+++ src/components/GeneralSettings/standings.jsx
@@ -7,13 +7,13 @@ function Standings(props) {
       <Box sx={{ minWidth: 120 }}>
         <FormControl sx={{ width: "33%" }}>
           <h3>How are Standings determined?</h3>
-          <TextField id="outlined-basic" variant="filled" />
+          <TextField id="standings" variant="filled" />
         </FormControl>
       </Box>
       <Box sx={{ minWidth: 120 }}>
         <FormControl sx={{ width: "33%" }}>
           <h3>Is tanking allowed?</h3>
-          <TextField id="outlined-basic" variant="filled" />
+          <TextField id="tanking" variant="filled" />
         </FormControl>
       </Box>
     </div>
--- src/components/GeneralSettings/trades.jsx
+++ src/components/GeneralSettings/trades.jsx
@@ -6,16 +6,16 @@ function Trades(props) {
     <Box>
       <FormControl>
         <h3>What is able to be traded in your league?</h3>
-        <TextField id="outlined-basic" variant="filled" />
+        <TextField id="tradeAsset" variant="filled" />
         <h3>Does your league allow trade vetos?</h3>
-        <Select>
+        <Select id="veto" variant="filled">
           <MenuItem value={"NoVeto"}>No Trade Vetos</MenuItem>
           <MenuItem value={"Yes"}>Allow Trade Vetos</MenuItem>
         </Select>
         <h3>Trade Colusion Settings</h3>
-        <TextField id="outlined-basic" variant="filled"></TextField>
+        <TextField id="colusion" variant="filled"></TextField>
         <h3>What do you do if Colusion is suspected?</h3>
-        <TextField id="outlined-basic" variant="filled" />
+        <TextField id="handleColusion" variant="filled" />
       </FormControl>
     </Box>
   );
