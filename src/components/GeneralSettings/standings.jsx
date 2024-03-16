a17f847707518f72827e5cc0d5d28fb55ec080cb
Andrew M <amess3309@gmail.com>
2024-03-15 23:35:04 -0500

    add value to standings

--- src/components/GeneralSettings/standings.jsx
+++ src/components/GeneralSettings/standings.jsx
@@ -7,13 +7,21 @@ function Standings(props) {
       <Box sx={{ minWidth: 120 }}>
         <FormControl sx={{ width: "33%" }}>
           <h3>How are Standings determined?</h3>
-          <TextField id="standings" variant="filled" />
+          <TextField
+            id="standings"
+            value={props.value.standings}
+            variant="filled"
+          />
         </FormControl>
       </Box>
       <Box sx={{ minWidth: 120 }}>
         <FormControl sx={{ width: "33%" }}>
           <h3>Is tanking allowed?</h3>
-          <TextField id="tanking" variant="filled" />
+          <TextField
+            id="tanking"
+            value={props.value.tanking}
+            variant="filled"
+          />
         </FormControl>
       </Box>
     </div>
