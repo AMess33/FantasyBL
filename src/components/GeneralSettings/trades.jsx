0d40903c549f49781c3a473fef7baf224cf1d6ed
Andrew M <amess3309@gmail.com>
2024-03-15 23:36:16 -0500

    add values to form fields in trades

--- src/components/GeneralSettings/trades.jsx
+++ src/components/GeneralSettings/trades.jsx
@@ -6,16 +6,28 @@ function Trades(props) {
     <Box>
       <FormControl>
         <h3>What is able to be traded in your league?</h3>
-        <TextField id="tradeAsset" variant="filled" />
+        <TextField
+          id="tradeAsset"
+          value={props.value.tradeAsset}
+          variant="filled"
+        />
         <h3>Does your league allow trade vetos?</h3>
-        <Select id="veto" variant="filled">
+        <Select id="veto" value={props.value.veto} variant="filled">
           <MenuItem value={"NoVeto"}>No Trade Vetos</MenuItem>
           <MenuItem value={"Yes"}>Allow Trade Vetos</MenuItem>
         </Select>
         <h3>Trade Colusion Settings</h3>
-        <TextField id="colusion" variant="filled"></TextField>
+        <TextField
+          id="colusion"
+          value={props.value.colusion}
+          variant="filled"
+        ></TextField>
         <h3>What do you do if Colusion is suspected?</h3>
-        <TextField id="handleColusion" variant="filled" />
+        <TextField
+          id="handleColusion"
+          value={props.value.handleColusion}
+          variant="filled"
+        />
       </FormControl>
     </Box>
   );
