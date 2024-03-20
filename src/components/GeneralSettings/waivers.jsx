c825294701527b3ef8c1046b238e422d3cb792a1
Andrew M <amess3309@gmail.com>
2024-03-19 23:28:01 -0500

    add onchange, remove imports, add styling

--- src/components/GeneralSettings/waivers.jsx
+++ src/components/GeneralSettings/waivers.jsx
@@ -1,28 +1,41 @@
 import React from "react";
-import { FormControl, Select, Box, MenuItem, TextField } from "@mui/material";
+import { Select, MenuItem, TextField } from "@mui/material";
 
 function Waivers(props) {
   return (
-    <Box>
-      <FormControl>
-        <h3>Waiver Settings</h3>
-        <Select id="waiver" value={props.value.waiver} variant="filled">
-          <MenuItem value={"FCFS"}>First Come First Serve</MenuItem>
-          <MenuItem value={"FAAB"}>
-            Free Agent Aquisition Budget Waivers
-          </MenuItem>
-          <MenuItem value={"Continual"}>
-            Continual Rolling List Waivers
-          </MenuItem>
-        </Select>
-        <h3>Explain any special waiver settings</h3>
-        <TextField
-          id="specialWaiver"
-          value={props.value.specialWaiver}
-          variant="filled"
-        />
-      </FormControl>
-    </Box>
+    <div style={{ width: "65%", margin: "auto" }}>
+      <h3>Waiver Settings</h3>
+      <Select
+        id="waiver"
+        value={props.value.waiver}
+        onChange={(event) => {
+          props.setValue((prevValues) => ({
+            ...prevValues,
+            waiver: event.target.value,
+          }));
+        }}
+        variant="filled"
+        fullWidth
+      >
+        <MenuItem value={"FCFS"}>First Come First Serve</MenuItem>
+        <MenuItem value={"FAAB"}>Free Agent Aquisition Budget Waivers</MenuItem>
+        <MenuItem value={"Continual"}>Continual Rolling List Waivers</MenuItem>
+      </Select>
+      <h3>Explain any special waiver settings</h3>
+      <TextField
+        id="specialWaiver"
+        sx={{ height: 20 }}
+        value={props.value.specialWaiver}
+        onChange={(event) => {
+          props.setValue((prevValues) => ({
+            ...prevValues,
+            specialWaiver: event.target.value,
+          }));
+        }}
+        variant="filled"
+        fullWidth
+      />
+    </div>
   );
 }
 
