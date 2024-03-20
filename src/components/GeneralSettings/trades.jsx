6d690a14148727c066dee2df7eea1491df67836b
Andrew M <amess3309@gmail.com>
2024-03-19 23:26:30 -0500

    add onchange, remove imports, add styling

--- src/components/GeneralSettings/trades.jsx
+++ src/components/GeneralSettings/trades.jsx
@@ -1,35 +1,68 @@
 import React from "react";
-import { FormControl, Select, Box, MenuItem, TextField } from "@mui/material";
+import { Select, MenuItem, TextField } from "@mui/material";
 
 function Trades(props) {
   return (
-    <Box>
-      <FormControl>
-        <h3>What is able to be traded in your league?</h3>
-        <TextField
-          id="tradeAsset"
-          value={props.value.tradeAsset}
-          variant="filled"
-        />
-        <h3>Does your league allow trade vetos?</h3>
-        <Select id="veto" value={props.value.veto} variant="filled">
-          <MenuItem value={"NoVeto"}>No Trade Vetos</MenuItem>
-          <MenuItem value={"Yes"}>Allow Trade Vetos</MenuItem>
-        </Select>
-        <h3>Trade Colusion Settings</h3>
-        <TextField
-          id="colusion"
-          value={props.value.colusion}
-          variant="filled"
-        ></TextField>
-        <h3>What do you do if Colusion is suspected?</h3>
-        <TextField
-          id="handleColusion"
-          value={props.value.handleColusion}
-          variant="filled"
-        />
-      </FormControl>
-    </Box>
+    <div>
+      <h3>What is able to be traded in your league?</h3>
+      <TextField
+        id="tradeAsset"
+        value={props.value.tradeAsset}
+        onChange={(event) => {
+          props.setValue((prevValues) => ({
+            ...prevValues,
+            tradeAsset: event.target.value,
+          }));
+        }}
+        variant="filled"
+        fullWidth
+      />
+      <h3>Does your league allow trade vetos?</h3>
+      <Select
+        id="veto"
+        sx={{ height: 20 }}
+        value={props.value.veto}
+        onChange={(event) => {
+          props.setValue((prevValues) => ({
+            ...prevValues,
+            veto: event.target.value,
+          }));
+        }}
+        variant="filled"
+        fullWidth
+      >
+        <MenuItem value={"NoVeto"}>No Trade Vetos</MenuItem>
+        <MenuItem value={"Yes"}>Allow Trade Vetos</MenuItem>
+      </Select>
+      <h3>Trade Colusion Settings</h3>
+      <TextField
+        id="colusion"
+        sx={{ height: 20 }}
+        value={props.value.colusion}
+        onChange={(event) => {
+          props.setValue((prevValues) => ({
+            ...prevValues,
+            colusion: event.target.value,
+          }));
+        }}
+        variant="filled"
+        fullWidth
+      ></TextField>
+      <h3>What do you do if Colusion is suspected?</h3>
+      <TextField
+        id="handleColusion"
+        sx={{ height: 20 }}
+        value={props.value.handleColusion}
+        onChange={(event) => {
+          props.setValue((prevValues) => ({
+            ...prevValues,
+            handleColusion: event.target.value,
+          }));
+        }}
+        variant="filled"
+        fullWidth
+      />
+    </div>
   );
 }
 
