61bbf0f3006e51342af78185aca3924750816878
Andrew M <amess3309@gmail.com>
2024-03-19 23:12:13 -0500

    add onchange functions to league type

--- src/components/InitialSetup/leagueType.jsx
+++ src/components/InitialSetup/leagueType.jsx
@@ -1,41 +1,45 @@
 import React from "react";
-import { FormControl, Select, Box, MenuItem } from "@mui/material";
+import { Select, MenuItem } from "@mui/material";
 
 function LeagueType(props) {
   return (
-    <div>
-      <Box>
-        <FormControl sx={{ width: "33%" }}>
-          <h3>What Type of Fantasy Contest</h3>
-          <Select
-            id="leagueType"
-            variant="filled"
-            value={props.value.LeagueType}
-          >
-            <MenuItem value={"Redraft"}>Seasonal Redraft</MenuItem>
-            <MenuItem value={"Dynasty"}>Dynasty</MenuItem>
-            <MenuItem value={"Keeper"}>Keeper</MenuItem>
-            <MenuItem value={"Playoff"}>Playoff Contest</MenuItem>
-            <MenuItem value={"Weekly"}>Weekly Contest</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
-      <Box>
-        <FormControl sx={{ width: "33%" }}>
-          <h3>Special League Type</h3>
-          <Select
-            id="specialLeagueType"
-            value={props.value.specialLeagueType}
-            variant="filled"
-          >
-            <MenuItem value={"Devy"}>Devy</MenuItem>
-            <MenuItem value={"Empire"}>Empire</MenuItem>
-            <MenuItem value={"Contract"}>Contract</MenuItem>
-            <MenuItem value={"BestBall"}>BestBall</MenuItem>
-            <MenuItem value={"Guillotine"}>Guillotine</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
+    <div style={{ width: "45%", margin: "auto" }}>
+      <h3>What Type of Fantasy Contest</h3>
+      <Select
+        id="leagueType"
+        variant="filled"
+        value={props.value.LeagueType}
+        onChange={(event) => {
+          props.setValue((prevValues) => ({
+            ...prevValues,
+            LeagueType: event.target.value,
+          }));
+        }}
+      >
+        <MenuItem value={"Redraft"}>Seasonal Redraft</MenuItem>
+        <MenuItem value={"Dynasty"}>Dynasty</MenuItem>
+        <MenuItem value={"Keeper"}>Keeper</MenuItem>
+        <MenuItem value={"Playoff"}>Playoff Contest</MenuItem>
+        <MenuItem value={"Weekly"}>Weekly Contest</MenuItem>
+      </Select>
+      <h3>Special League Type</h3>
+      <Select
+        id="specialLeagueType"
+        value={props.value.specialLeagueType}
+        onChange={(event) => {
+          props.setValue((prevValues) => ({
+            ...prevValues,
+            specialLeagueType: event.target.value,
+          }));
+        }}
+        variant="filled"
+      >
+        <MenuItem value={"Devy"}>Devy</MenuItem>
+        <MenuItem value={"Empire"}>Empire</MenuItem>
+        <MenuItem value={"Contract"}>Contract</MenuItem>
+        <MenuItem value={"BestBall"}>BestBall</MenuItem>
+        <MenuItem value={"Guillotine"}>Guillotine</MenuItem>
+      </Select>
     </div>
   );
 }
