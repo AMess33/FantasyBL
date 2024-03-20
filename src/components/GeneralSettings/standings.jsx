a36d2a1ea6ccd87bc45109dc9158b8af51119eb2
Andrew M <amess3309@gmail.com>
2024-03-19 23:24:26 -0500

    add on change remove unused imports, add styling

--- src/components/GeneralSettings/standings.jsx
+++ src/components/GeneralSettings/standings.jsx
@@ -1,29 +1,38 @@
 import React from "react";
-import { FormControl, Box, TextField } from "@mui/material";
+import { TextField } from "@mui/material";
 
 function Standings(props) {
   return (
     <div>
-      <Box sx={{ minWidth: 120 }}>
-        <FormControl sx={{ width: "33%" }}>
-          <h3>How are Standings determined?</h3>
-          <TextField
-            id="standings"
-            value={props.value.standings}
-            variant="filled"
-          />
-        </FormControl>
-      </Box>
-      <Box sx={{ minWidth: 120 }}>
-        <FormControl sx={{ width: "33%" }}>
-          <h3>Is tanking allowed?</h3>
-          <TextField
-            id="tanking"
-            value={props.value.tanking}
-            variant="filled"
-          />
-        </FormControl>
-      </Box>
+      <h3>How are Standings determined?</h3>
+      <TextField
+        id="standings"
+        sx={{ height: 20 }}
+        value={props.value.standings}
+        onChange={(event) => {
+          props.setValue((prevValues) => ({
+            ...prevValues,
+            standings: event.target.value,
+          }));
+        }}
+        variant="filled"
+        fullWidth
+      />
+
+      <h3>Is tanking allowed?</h3>
+      <TextField
+        id="tanking"
+        sx={{ height: 20 }}
+        value={props.value.tanking}
+        onChange={(event) =>
+          props.setValue((prevValues) => ({
+            ...prevValues,
+            tanking: event.target.value,
+          }))
+        }
+        variant="filled"
+        fullWidth
+      />
     </div>
   );
 }
