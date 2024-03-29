361030215072654a166a167285752c50c6bb4d3d
Andrew M <amess3309@gmail.com>
2024-03-28 21:35:31 -0500

    start icon functions for each accoridion on app page

--- src/components/icon.jsx
+++ src/components/icon.jsx
@@ -1,2 +1,18 @@
 import React from "react";
 import { CheckCircleIcon, RadioButtonUncheckedIcon } from "@mui/icons-material";
+
+export function DetailsIcon(props) {
+  if (props.isComplete) {
+    return <CheckCircleIcon />;
+  } else {
+    return <RadioButtonUncheckedIcon />;
+  }
+}
+
+export function MoneyIcon(props) {
+  if (props.isComplete) {
+    return <CheckCircleIcon />;
+  } else {
+    return <RadioButtonUncheckedIcon />;
+  }
+}
